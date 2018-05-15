d1cecb575294a55646ce652da7b5ede9b1b9a118
AderbalFilho <aderbalcrato@yahoo.com.br>
2018-05-14 21:46:47 -0300

    Adição de ementas

--- js/disciplinas.js
+++ js/disciplinas.js
@@ -6,39 +6,13 @@ function close() {
 	$(".course").hide();
 	$("#course-description-tip").show();
 }
-function repetitions(id) {
-	$(".course").hide();
-	$(id).show();
-	$(document).scrollTop( $("#course-description").offset().top );
-}
 close();
 $(".close").click(function() {
 	close();
 });
-$("#calculo").click(function() {
-	repetitions("#course-description-calculo");
-});
-$("#discreta").click(function() {
-	repetitions("#course-description-discreta");
-});
-$("#bdi").click(function() {
-	repetitions("#course-description-bdi");
-});
-$("#bdii").click(function() {
-	repetitions("#course-description-bdii");
-});
-$("#estatistica").click(function() {
-	repetitions("#course-description-estatistica");
-});
-$("#ia").click(function() {
-	repetitions("#course-description-ia");
-});
-$("#cg").click(function() {
-	repetitions("#course-description-cg");
-});
-$("#contabilidade").click(function() {
-	repetitions("#course-description-contabilidade");
-});
-$("#ingles").click(function() {
-	repetitions("#course-description-ingles");
+$(".course-clicks td").click(function() {
+	var id = $(this).attr('id');
+	$(".course").hide();
+	$("#course-description-"+id).show();
+	$(document).scrollTop( $("#course-description").offset().top );
 });
